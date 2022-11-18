99eda4328d9312108c326a2d7fa15399b214d7db
eiiti <eiiti@mail.ru>
2022-11-18 20:53:51 +0300

    Practice 6

---
+++ Quiz1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите кол-во чисел");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int[] a = new int[n];
+for(int i=0; i<n; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+    if(a[i]>0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine($"Количество положительных чисел: {sum}");
